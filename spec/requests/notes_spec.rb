0d2e0d8416c7f9f8be37b3e13e49a15bf3966378
rootlch <louish55@gmail.com>
2014-04-18 10:43:15 +0800

    Add test for cv id

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -11,11 +11,11 @@ describe "Notes" do
 
     before { visit root_path }
     it { should have_selector('h1', text: 'CV') }
-    it { should have_selector('h2', text: "Personal Details") }
-    it { should have_selector('h2', text: "Education and Qualification") }
-    it { should have_selector('h2', text: "Work Experience") }
-    it { should have_selector('h2', text: "Skills") }
-    it { should have_selector('h2', text: "Objectives") }
+    it { should have_selector('h2#personal', text: "Personal Details") }
+    it { should have_selector('h2#education', text: "Education and Qualification") }
+    it { should have_selector('h2#experience', text: "Work Experience") }
+    it { should have_selector('h2#skills', text: "Skills") }
+    it { should have_selector('h2#objectives', text: "Objectives") }
     it { should have_content("Reference available upon request") }
 
     describe "variable" do
